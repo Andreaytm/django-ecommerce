71382f5986d7d7f619ada8a90ffa9f0fb8a3f8df
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-10-06 15:58:26 +0000

    added stripe javascript to allow for payment of products

--- checkout/views.py
+++ checkout/views.py
@@ -5,10 +5,10 @@ from django.conf import settings
 from django.utils import timezone
 from .forms import MakePaymentForm, OrderForm
 from .models import OrderLineItem
-from product.models import Product
+from products.models import Product
 import stripe
 
-# Create your views here.
+# Create your views here.d
 stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
@@ -16,6 +16,7 @@ def checkout(request):
     if request.method=="POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
+        
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
@@ -25,7 +26,7 @@ def checkout(request):
             total = 0
             for id, quantity in cart.items():
                 product = get_object_or_404(Product, pk=id)
-                total += quantity * product.prices
+                total += quantity * product.price
                 order_line_item = OrderLineItem(
                     order = order,
                     product = product, 
@@ -39,18 +40,16 @@ def checkout(request):
                     currency = "GBP",
                     description = request.user.email,
                     card = payment_form.cleaned_data['stripe_id'],
-                    )
+                )
             except stripe.error.CardError:
-                messages.error(request, "Sorry, your card was declined.")
+                messages.error(request, "Your card was declined")
                 
             if customer.paid:
                 messages.error(request, "You have successfully paid!")
-                request.session['cart'] ={}
+                request.session['cart'] = {}
                 return redirect(reverse('products'))
-                
             else:
-                messages.error(request, "Unable to take payment.")
-                
+                messages.error(request, "Unable to take payment")
         else:
             print(payment_form.errors)
             messages.error(request, "We were unable to take a payment with that card.")
